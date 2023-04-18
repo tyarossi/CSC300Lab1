65d7ff2c8d25a221090cb3207da3c22f27d246e2
Thomas Urbanski <tjurbanski@comcast.net>
2023-04-18 09:17:49 -0400

    Made Refund and Purchase Tickets only accessable from the ticketViewing page. Made TicketViewing Page only viewable when logged into an account

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,10 +11,10 @@ import LandingPage from "./components/pages/landingPage";
 import HomePage from "./components/pages/homePage";
 import Login from "./components/pages/loginPage";
 import Signup from "./components/pages/registerPage";
-import Ticketing from "./components/pages/buyTicket";
+import BuyTickets from "./components/pages/buyTicket";
 import PrivateUserProfile from "./components/pages/privateUserProfilePage";
 import EditUserProfile from "./components/pages/editUserPage";
-import TicketViewingPage from "./components/pages/ticketViewingPage";
+import Ticketing from "./components/pages/ticketViewingPage";
 import TestTJ from "./components/pages/testTJpage";
 import TestCS from "./components/pages/testCSpage";
 import TestTY from "./components/pages/testTYpage";
@@ -43,10 +43,10 @@ const App = () => {
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
           <Route exact path="/mbtaAlerts" element={<MbtaAlertsPage />} />
-          <Route exact path="/ticketing" element={<Ticketing />} />
+          <Route exact path="/buyTicket" element={<BuyTickets />} />
           <Route exact path="/mbtaTrip" element={<MbtaTrip />} />
           <Route exact path="/trainTrackerPage" element={<trainTrackerPage />} />
-          <Route exact path="/ticketViewingPage" element={< TicketViewingPage/>} />
+          <Route exact path="/ticketViewingPage" element={< Ticketing/>} />
           <Route exact path="/ticketRefundingPage" element={< Refunding/>} />
 
           <Route path="/privateUserProfile" element={<PrivateUserProfile />} />
--- frontend/src/components/pages/ticketRefundingPage.js
+++ frontend/src/components/pages/ticketRefundingPage.js
@@ -1,114 +1,96 @@
-import React, { useState, useEffect } from 'react';
-import Card from 'react-bootstrap/Card';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import FormText from 'react-bootstrap/esm/FormText';
+import { useNavigate } from "react-router-dom";
 
-
-// const PRIMARY_COLOR = "#cc5c99";
-// const SECONDARY_COLOR = "#0c0c1f";
-
-// const CreateTicket = () => {
-//   const [light, setLight] = useState(false);
-//   const [bgColor, setBgColor] = useState(SECONDARY_COLOR);
-//   const [bgText, setBgText] = useState("Light Mode");
-// }
-
-// useEffect(() => {
-//   if (light) {
-//     setBgColor("white");
-//     setBgText("Dark mode");
-//   } else {
-//     setBgColor(SECONDARY_COLOR);
-//     setBgText("Light mode");
-//   }
-// }, [light]);
-
-
+const url = "http://localhost:8081/tickets/deleteTicket";
 
 function Refunding() {
-  
+  const [data, setData] = useState({ ticketnum: ""});
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    return (
-      
-  
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data: res } = await axios.post(url, data);
+      const {accessToken} = res
+      //get accesstoken stored on device
+      localStorage.getItem("accessToken", accessToken)
+      navigate("/ticketViewingPage");
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
+  return (
     <Form>
-    
-    <h1> 
-        Welcome to MBTA Refunding Page
-      </h1>
-        
-
-        
-        
+    <h1>Welcome to MBTA Refunding Page</h1>
         <Form.Group className="mb-3" controlId="Username" >
           <Form.Label>Username</Form.Label>
-         
-          <Form.Control type="username" placeholder="Please Enter Your Username" />
-          
-          
-         </Form.Group>
+          <Form.Control 
+            type="username" 
+            name="username" 
+            placeholder="Please Enter Your Username" /> 
+        </Form.Group>
   
         <Form.Group className="mb-3" controlId="Password">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Please Enter Your Password" />
-            </Form.Group>
+          <Form.Control 
+            type="password" 
+            placeholder="Please Enter Your Password" />
+        </Form.Group>
 
-            <Form.Group className="mb-3" controlId="ConfirmationNum">
+        <Form.Group className="mb-3" controlId="ConfirmationNum">
           <Form.Label>Confirmation Number</Form.Label>
-          <Form.Control type="confirmNum" placeholder="PLease Enter Your Ticket Confirmation Number" />
-          </Form.Group>
+          <Form.Control 
+            type="ticketnum"  
+            name="ticketnum"  
+            onChange={handleChange}
+            placeholder="Please Enter Your Ticket Confirmation Number"/>
+        </Form.Group>
 
-          <Form.Group className="mb-3" controlId="PaymentInfo">
+        <Form.Group className="mb-3" controlId="PaymentInfo">
           <Form.Label>Payment Information</Form.Label>
-          <Form.Control type="paymentInfo" placeholder="Please Enter Your Payment Information" />
+          <Form.Control 
+            type="paymentInfo" 
+            placeholder="Please Enter Your Payment Information" />
           <Form.Text className="text-muted">
             We'll never share your payment information with anyone else.
           </Form.Text>
-          </Form.Group>
-
-
+        </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="I am sure I want to cancel my ticket" />
-        
+          <Form.Check 
+            type="checkbox" 
+            label="I am sure I want to cancel my ticket" />
         </Form.Group>
-        <Button variant="primary" type="Submit">
-          Submit
+        
+        {error && (
+          <div className="pt-3">
+            {error}
+          </div>
+        )}
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={handleSubmit}
+          className="mt-2"
+        >
+          Confirm
         </Button>
       </Form>
     );
   }
-
-  
-  
-  export default Refunding;
-
-    
-    
-
-
-//   function Refunding() {
-  
-//     return (
-//         <button>
-//             Cancel
-//         </button>   
-//     );
-// }
-    
-//         <div>
-//         <h1>
-//         style={{color:'white', fontSize:'150', fontWeight:'bold',flex:1,justifyContent: 'right', alignItems: 'right',  textAlign: 'center',fontWeight:'bold' }}
-//         Welcome to MBTA Ticket Refunding Page </h1>
-//         <h2>style={{color:'#b342f5', fontSize:'50', fontWeight:'bold',content:'center',textAlign: 'center' }} 
-//         Please Enter Your Username, Password and Ticket Confirmation Number</h2>
-        
-       
-//         </div>
-    
-
-
-
-
+export default Refunding;
